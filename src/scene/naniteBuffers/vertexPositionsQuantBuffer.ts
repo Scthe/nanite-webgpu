7e59835a146d2de3756a234679393d8f876becf8
Scthe <marcin1113C@gmail.com>
2024-07-06 17:36:56 +0200

    Vertex position quantization done. Decide if add it to rest of app?

---
+++ src/scene/naniteBuffers/vertexPositionsQuantBuffer.ts
@@ -0,0 +1,132 @@
+import { vec3 } from 'wgpu-matrix';
+import { calcBoundingBox } from '../../utils/calcBounds.ts';
+import { createGPUBuffer } from '../../utils/webgpu.ts';
+
+/*
+THE ALGORITHM WORKS, BUT IS CURRENTLY UNUSED.
+
+You need to provide 2 dequantization constants (6 floats per object)
+into the shader, which means storing them somewhere in some GPU buffer.
+Too much hassle for me.
+
+To prove that this works, you can render a single object
+and hardcode the constants for this one object into the WGSL.
+Don't forget to actaully call this code (just replace
+all usages of `vertexPositionsBuffer.ts`), it's used 2 times in entire codebase.
+One when writing to the GPU buffer (`createNaniteVertexPositionsBuffer()`)
+and then replace WGSL snippet (`BUFFER_VERTEX_POSITIONS`).
+
+
+# TO FINISH:
+
+Provide into the shader 2 values that are generated during quantization:
+- let dequantFactor = _somePerObjectData.dequantFactor;
+- let dequantSummand = _somePerObjectData.dequantSummand;
+
+The values are printed into the console at the end of 
+`createNaniteVertexPositionsQuantBuffer()`.
+
+# BASED ON:
+
+- https://momentsingraphics.de/ToyRenderer2SceneManagement.html#Quantization
+- https://github.com/MomentsInGraphics/vulkan_renderer/blob/38b1dc04ec3e67a311dad2f8e863eb070d076135/tools/io_export_vulkan_blender28.py#L481
+- https://github.com/MomentsInGraphics/vulkan_renderer/blob/38b1dc04ec3e67a311dad2f8e863eb070d076135/src/scene.h#L56
+
+*/
+
+///////////////////////////
+/// SHADER CODE
+/// TODO [CRITICAL] Decide if release this or add optional GUI switch etc.
+///////////////////////////
+
+export const BUFFER_VERTEX_POSITIONS_QUANT = (
+  bindingIdx: number
+) => /* wgsl */ `
+
+@group(0) @binding(${bindingIdx})
+var<storage, read> _vertexPositions: array<vec2u>;
+
+/** get vertex position. Has .w=1 */
+fn _getVertexPosition(idx: u32) -> vec4f {
+  let dequantFactor = _objectData.dequantFactor;
+  let dequantSummand = _objectData.dequantSummand;
+  let positionQuant = _vertexPositions[idx];
+
+  let positionU32 = vec3u(
+    positionQuant[0] & 0x1FFFFF,
+    ((positionQuant[0] & 0xFFE00000) >> 21) | ((positionQuant[1] & 0x3FF) << 11),
+    (positionQuant[1] >> 10) & 0x1FFFFF
+  );
+  let positionF32 = vec3f(positionU32) * dequantFactor + dequantSummand;
+  return vec4f(positionF32.xyz, 1.0);
+}
+`;
+
+///////////////////////////
+/// GPU BUFFER
+///////////////////////////
+
+export function createNaniteVertexPositionsQuantBuffer(
+  device: GPUDevice,
+  name: string,
+  vertices: Float32Array
+): GPUBuffer {
+  const vertexCount = vertices.length / 3;
+  // Quantization: calculate bounding box and split into 2**21 'places' per axis.
+  // 3 axis * 21bytes = 63bytes for total vertex position.
+  // The comments refer to Christoph Peters's
+  // https://github.com/MomentsInGraphics/vulkan_renderer original code.
+  const QUANT_PLACES = 2.0 ** 21.0;
+
+  const [bboxMin, bboxMax] = calcBoundingBox(vertices);
+  const bboxSpan = vec3.sub(bboxMax, bboxMin);
+  // PY: quantization_factor = (2.0**21.0 / (box_max - box_min))
+  const quantFactor = vec3.create(
+    QUANT_PLACES / bboxSpan[0],
+    QUANT_PLACES / bboxSpan[1],
+    QUANT_PLACES / bboxSpan[2]
+  );
+  // PY: quantization_offset = -box_min * quantization_factor
+  const quantOffset = vec3.mul(bboxMin, vec3.negate(quantFactor));
+
+  const vertexPos = vec3.create();
+  const quantVertPos = (x: number) => Math.floor(Math.min(x, QUANT_PLACES - 1));
+
+  const data = new Uint32Array(vertexCount * 2);
+
+  for (let i = 0; i < vertexCount; i++) {
+    vec3.set(
+      vertices[i * 3],
+      vertices[i * 3 + 1],
+      vertices[i * 3 + 2],
+      vertexPos
+    );
+    // PY: quantized_positions = np.asarray(mesh.vertex_position * quantization_factor + quantization_offset, dtype=np.uint32)
+    vec3.mul(vertexPos, quantFactor, vertexPos);
+    vec3.add(vertexPos, quantOffset, vertexPos);
+    // PY: quantized_positions = np.minimum(2**21 - 1, quantized_positions)
+    const vertexPosQuantX = quantVertPos(vertexPos[0]); // 21 bits
+    const vertexPosQuantY = quantVertPos(vertexPos[1]); // 21 bits
+    const vertexPosQuantZ = quantVertPos(vertexPos[2]); // 21 bits
+
+    data[i * 2 + 0] = vertexPosQuantX;
+    data[i * 2 + 0] += (vertexPosQuantY & 0x7ff) << 21; // write 11 bits of axis Y
+    data[i * 2 + 1] = (vertexPosQuantY & 0x1ff800) >> 11; // write remaining 10 bits of axis Y to other u32
+    data[i * 2 + 1] += vertexPosQuantZ << 10;
+  }
+
+  // PY: dequantization_factor = 1.0 / quantization_factor
+  const dequantFactor = vec3.inverse(quantFactor); // we will revert the scaling
+  // PY: box_min + 0.5 * quantFactor;
+  const dequantSummand = vec3.addScaled(bboxMin, dequantFactor, 0.5);
+
+  // TODO [QUANT] provide both values to the WGSL's _getVertexPosition()
+  console.log({ dequantFactor, dequantSummand });
+
+  return createGPUBuffer(
+    device,
+    `${name}-nanite-vertex-buffer-vec4`,
+    GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+    data
+  );
+}
